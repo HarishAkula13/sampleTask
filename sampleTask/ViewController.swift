7abc9f7393e213f11eece57f599afbc4353ac946
surendranath reddy <surendranathreddy@surendranaths-MacBook-Air.local>
2021-03-13 18:14:47 +0530

    first commit

---
+++ sampleTask/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  sampleTask
+//
+//  Created by surendranath reddy on 04/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        var a:UInt16 = 2
+        
+        while a<=100
+        {
+
+         print(a)
+            a += 2
+        }
+ 
+        for i in 
+        
+        
+        }
+        
+        
+        
+    }
+     
+
+
+
